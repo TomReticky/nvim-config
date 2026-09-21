bf0fbcf39d23157fd5bcacfb9f743b4f0d6b68dc
TomReticky <tomas.reticky@gmail.com>
2026-09-21 12:15:59 +0200

    added avante

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = 'a'
@@ -28,39 +31,29 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
-    "milanglacier/minuet-ai.nvim",
-    config = function()
-      require("minuet").setup({
-        virtualtext = {
-          auto_trigger_ft = {},
-          keymap = {
-            accept_line = "<A-a>",
-            dismiss = "<A-e>",
-            next = "<A-c>",
-          },
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "openrouter",
+      providers = {
+        openrouter = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "deepseek/deepseek-v4-flash",
+          max_tokens = 10240,
         },
-        provider = 'openai_compatible',
-        request_timeout = 2.5,
-        throttle = 1500,
-        debounce = 600,
-        provider_options = {
-          openai_compatible = {
-            api_key = 'OPENROUTER_API_KEY',
-            end_point = 'https://openrouter.ai/api/v1/chat/completions',
-            model = 'deepseek/deepseek-v4-flash',
-            name = 'Openrouter',
-            optional = {
-              max_tokens = 128,
-              top_p = 0.9,
-              provider = {
-                sort = 'throughput',
-              },
-              reasoning_effort = 'none'
-            },
-          },
-        },
-      })
-    end,
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", 
+    },
   },
   {
     "rose-pine/neovim",
@@ -92,4 +85,3 @@ require("lazy").setup({
     end,
   }
 })
-
